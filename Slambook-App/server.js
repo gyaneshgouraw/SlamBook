dee1a669bc4d4cde1c83653894b0c6947d672054
Gyanesh Gouraw <gyaneshgouraw@gmail.com>
2015-12-31 15:36:07 +0530

    Server console messgae modified

--- Slambook-App/server.js
+++ Slambook-App/server.js
@@ -39,7 +39,7 @@ require('./app/routes')(app); // pass our application into our routes
 //======================>end<===============================
 // start app ===============================================
 app.listen(port);	
-console.log('Magic happens on port ' + port); 			// shoutout to the user
+console.log('Slambook will make magic happens on port ' + port); 			// shoutout to the user
 exports = module.exports = app; 						// expose app
 
 
