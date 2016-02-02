7a5fa2cfcc5558483815753c666dc3ab33203e70
Gyanesh Gouraw <gyaneshgouraw@gmail.com>
2016-02-03 00:37:47 +0530

    deploy v2

--- app/routes.js
+++ app/routes.js
@@ -7,17 +7,18 @@ var nodemailer = require('nodemailer');
 function getCustomerCollection(res){
 	CustomerCollection.find(function(err, cust) {
 			if (err)
-				res.send(err)
+				res.send(err);
 			res.json(cust); // return all todos in JSON format
 		});
 };
 function getSlambookollection(res){
 	Slambookollection.find(function(err, slam) {
 			if (err)
-				res.send(err)
+				res.send(err);
 			res.json(slam); // return all todos in JSON format
 		});
 };
+
 
 module.exports = function(app) {
 
@@ -63,11 +64,35 @@ module.exports = function(app) {
 app.get('/api/slambookollection', function (req, res) {
 		getSlambookollection(res);
 	});
+
+app.post('/api/slambookollection/getrecordbyemail', function (req, res) {
+			Slambookollection.find({ sendermailid: req.body.sendermailid,recievermailid: req.body.recievermailid}, function(err, character) {
+				//console.log(res); // { name: 'Frodo', inventory: { ringOfPower: 1 }}
+				if (err)
+				res.send(err);
+			return res.json(character);
+			
+			});
+	});
+
+
 app.post('/api/slambookollection', function (req, res) {
 		Slambookollection.create(req.body, function(err, cust) {
 			if (err)
 				res.send(err);
 			getSlambookollection(res);
+
+    var maildata = 	{
+					"data": "Your friend "+req.body.recievername+" has filled slambook for you.",
+					"senderid": req.body.recieverKey,
+					"sendermailid":req.body.recievermailid,
+					"recieverid": req.body.senderKey ,
+					"recievermailid": req.body.sendermailid,
+					"reciever": req.body.sendermailid,
+					"flag":"slamsubmit" 
+						};
+
+			handleSayHello(maildata,res);
 		});
 	});
 
@@ -98,16 +123,35 @@ function handleSayHello(req, res) {
             pass: 'debug@apple' // Your password
         }
     });
-
-    var mailOptions = {
+var mailOptions ={};
+if(req && req.flag =="slamsubmit"){
+	 mailOptions = {
+    from: 'gyaneshgouraw110899@gmail.com', // sender address//jitibisht.bisht@gmail.com
+    to: req.reciever, // list of receivers//mohanmanis@gmail.com
+    subject: 'Slam Book Filled', // Subject line
+    text: 'Slambook link', //, // plaintext body
+    html:req.data+"</br>"+
+     "<a href=http://localhost:3003/login/disp/slam/"+req.recievermailid+"/"+req.sendermailid+"> Click to visit filled slambook page✔</a>" // You can choose to send an HTML body instead
+};
+	transporter.sendMail(mailOptions, function(error, info){
+	    if(error){
+	        console.log(error);
+	        res.json({yo: 'error'});
+	    }else{
+	        console.log('Message sent: ' + info.response);
+	       // res.json({yo: info.response});
+	    }
+	});
+}
+	else{
+		  mailOptions = {
     from: 'gyaneshgouraw110899@gmail.com', // sender address//jitibisht.bisht@gmail.com
     to: req.body.reciever, // list of receivers//mohanmanis@gmail.com
     subject: 'Please Fill my slam book', // Subject line
     text: 'Slambook link', //, // plaintext body
     html:req.body.data+"</br>"+
-     "<a href=http://localhost:3003/login/"+ req.body.senderid+"/"+req.body.recieverid+"> Click to visit slambook page✔</a>" // You can choose to send an HTML body instead
+     "<a href=http://localhost:3003/login/"+ req.body.senderid+"/"+req.body.recieverid+"/"+req.body.sendermailid+"/"+req.body.recievermailid+"> Click to visit slambook page✔</a>" // You can choose to send an HTML body instead
 };
-
 	transporter.sendMail(mailOptions, function(error, info){
 	    if(error){
 	        console.log(error);
@@ -115,8 +159,12 @@ function handleSayHello(req, res) {
 	    }else{
 	        console.log('Message sent: ' + info.response);
 	        res.json({yo: info.response});
-	    };
+	    }
 	});
+	}
+   
+
+
 }
 
 
